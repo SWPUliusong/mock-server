ad685564c6021245ca23b53f3f9ef127af2eb7d9
liusong <swpuliusong@foxmail.com>
2018-03-15 17:32:48 +0800

    update

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -11,11 +11,6 @@ export default new Router({
       component: require('@/components/Home').default
     },
     {
-      path: '/projects/:id',
-      name: 'project',
-      component: require('@/components/Home').default
-    },
-    {
       path: '*',
       redirect: '/'
     }
